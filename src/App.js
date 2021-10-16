d9c260c4ad0bf78d1463042b5f809ec8e93583b8
danainjax <danainjax@gmail.com>
2021-10-16 12:20:44 -0400

    redesigned to a Disney blog format

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css'
 import BlogPost from './components/BlogPost'
-import Candystore from './components/Candystore'
 
-function FunkyChicken(props) {
+function Rides(props) {
+  console.log(props)
   return (
-    <>
-      <h2>Funky Freaking Chicken, diversity-boy!</h2>
+    <div>
+      <h4>These are the best Disney Rides</h4>
       <ul>
         {props.rides.map((ride) => (
-          <li>{ride}</li>
+          <li key={ride.id}>{ride.title}</li>
         ))}
       </ul>
-    </>
+    </div>
   )
 }
 const rides = [
@@ -21,12 +21,17 @@ const rides = [
   'Avatar Ride',
 ]
 
+const ridesObjects = rides.map((ride, i) => ({ id: i, title: ride }))
+console.log(ridesObjects)
 function App() {
   return (
     <div>
-      <Candystore name="See's Candies" candy='chocolates' />
-      <FunkyChicken rides={rides} />
-      <h1>Thought Patterns Blog</h1>
+      <h1>Disney Blog</h1>
+      <Rides rides={ridesObjects} />
+      {/* rides is the key in an array of rides objects, so when you iterate over it
+      above, you need to call props (which is the array, then the key, then keep
+      going) */}
+
       <BlogPost />
     </div>
   )
